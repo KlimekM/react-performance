d8daed5ae452e90e9acc141692eaa9467c3a7a78
Michal Klimek <mklimek15@gmail.com>
2022-02-03 20:03:28 -0600

    03: complete React.memo custom comparator extra credit

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -31,8 +31,8 @@ function Menu({
   )
 }
 
-Menu = React.memo(Menu)
 // 🐨 Memoize the Menu here using React.memo
+Menu = React.memo(Menu)
 
 function ListItem({
   getItemProps,
@@ -59,8 +59,27 @@ function ListItem({
   )
 }
 
-ListItem = React.memo(ListItem)
 // 🐨 Memoize the ListItem here using React.memo
+ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+  // true means do NOT rerender
+  // false means DO rerender
+
+  // these ones are easy if any of these changed, we should re-render
+  if (prevProps.getItemProps !== nextProps.getItemProps) return false
+  if (prevProps.item !== nextProps.item) return false
+  if (prevProps.index !== nextProps.index) return false
+  if (prevProps.selectedItem !== nextProps.selectedItem) return false
+
+  // this is trickier. We should only re-render if this list item:
+  // 1. was highlighted before and now it's not
+  // 2. was not highlighted before and now it is
+  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+    const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
+    const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
+    return wasPrevHighlighted === isNowHighlighted
+  }
+  return true
+})
 
 function App() {
   const forceRerender = useForceRerender()
